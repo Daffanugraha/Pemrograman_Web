cb4ade784d4a5f9f5909830b8d2eed3bc5124711
Daffanugraha <127364724+Daffanugraha@users.noreply.github.com>
2023-11-30 08:06:52 +0700

    Add files via upload

---
+++ latihan_modul5/app/Routes/ProductRoutes.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\Routes;
+
+include "Controller/ProductController.php";
+
+use app\Controller\ProductController;
+
+class ProductRoutes{
+    public function handle($method, $path){
+        if ($method == "GET" && $path == "/api/product") {
+            $controller = new ProductController();
+            echo $controller->index();
+        } else {
+            // Tambahkan pernyataan debug di sini
+            error_log("Path tidak sesuai: $path");
+        }
+        
+
+        if($method == "GET" && strpos($path, "/api/product/") == 0){
+            
+            $pathParts = explode('/', $path);
+            $id = $pathParts[count($pathParts) - 1];
+
+            $controller = new ProductController();
+            echo $controller->getById($id);
+        }
+
+        if($method == "POST" && $path == "/api/product"){
+            $controller = new ProductController();
+            echo $controller->insert();
+        }
+
+        if($method == "PUT" && strpos($path, "/api/product/") == 0){
+
+            $pathParts = explode('/', $path);
+            $id = $pathParts[count($pathParts) - 1];
+
+            $controller = new ProductController();
+            echo $controller->update($id);
+        }
+
+        if($method == "DELETE" && strpos($path, "/api/product/") == 0){
+
+            $pathParts = explode('/', $path);
+            $id = $pathParts[count($pathParts) - 1];
+
+            $controller = new ProductController();
+            echo $controller->delete($id);
+        }
+    }
+}
